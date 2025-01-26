c4d88ba0bebce135847d3db6ad9526be3354d775
Carlos Silva <cvcardos5@gmail.com>
2025-01-26 12:53:36 -0300

    Built web
    Branch: main
    Commit: 47c3bb02a5d26b836c797d5fa62f189f668dc0f9
    
    package:peanut 5.1.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "466268411"
+    serviceWorkerVersion: "1435683122"
   }
 });
